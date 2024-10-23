24f0e060b3555f4c05b841cfa5ef10e47de8147d
Vikash Singh <vikash97.singh19@gmail.com>
2024-10-23 14:29:54 -0500

    first commit

---
+++ sample.py
@@ -0,0 +1,26 @@
+import os
+
+
+def greet_user(name):
+    """Greets the user"""
+    if name:
+        print(f"Hello, {name}!")
+    else:
+        print("Hello, Guest!")
+
+
+# This is a function that tries to open an environment variable
+def get_secret_key():
+    """Gets the secret key from environment variable"""
+    return os.getenv("SECRET_KEY")
+
+
+# Main function
+if __name__ == "__main__":
+    user_name = input("Enter your name: ")
+    greet_user(user_name)
+    secret_key = get_secret_key()
+    if secret_key:
+        print("Secret key is found!")
+    else:
+        print("No secret key found.")
